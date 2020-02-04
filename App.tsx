4ea07a9ffe27a8d6d62a51f2df7427e2d512f619
rahul.gollapudi <rahul.gollapudi@varchas.net>
2020-02-04 16:29:41 +0000

    Barcode scanner

--- App.tsx
+++ App.tsx
@@ -5,11 +5,13 @@ import { mapping, light as lightTheme } from "@eva-design/eva";
 import { BottomNavigator } from "./src/nav/BottomNavigator";
 import StackNavigator from "./src/nav/StackNavigator";
 import AppSplash from "./src/modules/app_splash/AppSplash";
-
+//import {  ScanScreen} from './src/modules/Qrcode/scan'
+import Scanner from './src/modules/Qrcode/sancode'
 export default function App() {
 	return (
 		<ApplicationProvider mapping={mapping} theme={lightTheme}>
 			<AppSplash />
+			<Scanner />
 			<StackNavigator />
 		</ApplicationProvider>
 	);
